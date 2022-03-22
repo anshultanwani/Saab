6a9d87ff1b2b367f890dc23d55c92fc641f181da
Meerchandani <Harsha.Meerchandani@philips.com>
2022-03-22 14:13:56 +0530

    home slider ui changes

---
+++ src/components/HomeSlider.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Slider from "react-slick";
+import { connect } from 'react-redux';
+import './home-slider.scss';
+import { Button } from '@mui/material';
+
+const HomeSlider = (props) => {
+        const settings = {
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        };
+        return (
+          <div>
+            <Slider {...settings}>
+            {
+            props.homesliderimages.map((image , index)=> {
+                    return (
+                    <div key={index}>
+                    <div className="sec-left">
+                    <img src={require('../assets/'+image.imgPath).default}/>
+                    </div>
+                    <div className="sec-right slidertext">
+                    <h1>{image.imgText}</h1>
+                    <Button 
+                    variant="outlined"
+                    className="slider-btn"
+                >
+                    Book Now
+                </Button>
+                    </div>
+                    </div>
+                    )
+            })}
+            </Slider>
+          </div>
+        );
+      }
+
+
+const mapStateToProps = state => {
+        return {
+            homesliderimages: state.foodData.homeslider
+        }
+}
+export default connect(mapStateToProps)(HomeSlider);
